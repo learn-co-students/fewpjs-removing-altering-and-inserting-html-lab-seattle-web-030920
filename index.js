fdaa234be917fd631dba9ebeb1a291824e078272
christine <bumpouce@gmail.com>
2020-04-19 19:43:46 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let body = document.querySelector("body");
+body.removeChild(body.querySelector('main#main'));
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Christine is the champion";
