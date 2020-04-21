d54309a5abed08f1852463b314df5cc01c6b963a
wakennedy <alexkenned@gmail.com>
2020-04-21 13:35:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let findMain = document.querySelector("main");
+findMain.remove()
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Will is the champion!";
